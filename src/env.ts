430abf0f8c3a48a7f17e3c8fa81f13a072022bb9
Gustavo <gustavopolonio1@gmail.com>
2024-05-15 17:53:09 -0300

    fetch api wrapper

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
+
+const parsedEvent = envSchema.safeParse(process.env)
+
+if (!parsedEvent.success) {
+  console.error(
+    'Invalid environment variables.',
+    parsedEvent.error.flatten().fieldErrors,
+  )
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = parsedEvent.data
